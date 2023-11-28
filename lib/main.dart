cf3fe9ff3f794aba51f012a49ebff7890688edd0
Vladislav Zyuzko <zyuzko142@gmail.com>
2023-11-29 01:56:56 +0600

    Добавлены шрифты

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:effective_flutter_lab/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      theme: returnMainTheme(),
+      home: const Scaffold(
+        body: Text(
+        "Hello World!",
+        style: TextStyle(
+          fontFamily: 'SF_Pro_Text',
+          fontWeight: FontWeight.w500,
+          fontSize: 48,
+        ),
+      ),
+      )
     );
   }
 }
