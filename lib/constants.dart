053c14f260b4d9ae8888505c25ec8c439e68ff27
Vladislav Zyuzko <zyuzko142@gmail.com>
2023-11-29 16:11:00 +0600

    Собрал все строки

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 
-const displaySmall = TextStyle(
-    fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w700, fontSize: 16);
+const interestsList = [
+  "Еда", "Самообразование", "Технологии", "Дом", "Досуг", "Забота о себе", "Наука",
+];
 
 abstract class AppFonts {
   static const titleLarge = TextStyle(
-      fontFamily: 'SF_Pro_Display', fontWeight: FontWeight.w700, fontSize: 24, color: AppColors.black);
+      fontFamily: 'SF_Pro_Display',
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: AppColors.black);
   static const titleMedium = TextStyle(
-      fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w700, fontSize: 20, color: AppColors.black);
+      fontFamily: 'SF_Pro_Text',
+      fontWeight: FontWeight.w700,
+      fontSize: 20,
+      color: AppColors.black);
   static const titleSmall = TextStyle(
-      fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w500, fontSize: 14, color: AppColors.black);
+      fontFamily: 'SF_Pro_Text',
+      fontWeight: FontWeight.w500,
+      fontSize: 14,
+      color: AppColors.black);
   static const bodyLarge = TextStyle(
-      fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w500, fontSize: 16, color: AppColors.black);
+      fontFamily: 'SF_Pro_Text',
+      fontWeight: FontWeight.w500,
+      fontSize: 16,
+      color: AppColors.black);
   static final bodyMedium = TextStyle(
-      fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w500, fontSize: 16, color: AppColors.black55);
+      fontFamily: 'SF_Pro_Text',
+      fontWeight: FontWeight.w500,
+      fontSize: 16,
+      color: AppColors.black55);
   static final bodySmall = TextStyle(
-      fontFamily: 'SF_Pro_Text', fontWeight: FontWeight.w500, fontSize: 14, color: AppColors.black55);
+      fontFamily: 'SF_Pro_Text',
+      fontWeight: FontWeight.w500,
+      fontSize: 14,
+      color: AppColors.black55);
 }
 
 abstract class AppColors {
